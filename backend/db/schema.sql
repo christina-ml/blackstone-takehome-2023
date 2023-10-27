a4dba487bee2bb390ae70ea44ec881e06470d83e
Christina Loiacono <loiaconochristina@gmail.com>
2023-10-27 17:27:10 -0400

    Create tables in schema.sql and seed data in seed.sql Create another .gitignore file for the backend

---
+++ backend/db/schema.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS meeting_room_bookings;
+CREATE DATABASE meeting_room_bookings;
+
+\c meeting_room_bookings;
+
+DROP TABLE IF EXISTS meeting_rooms;
+
+CREATE TABLE meeting_rooms (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    capacity SMALLINT,
+    floor SMALLINT
+);
+
+DROP TABLE IF EXISTS bookings;
+
+CREATE TABLE bookings (
+    id SERIAL PRIMARY KEY,
+    meeting_name VARCHAR(255) UNIQUE NOT NULL,
+    meeting_room_id INTEGER REFERENCES meeting_rooms(id) ON DELETE CASCADE,
+    start_date TIMESTAMP NOT NULL,
+    end_date TIMESTAMP NOT NULL CHECK (start_date < end_date),
+    attendees TEXT
+);
---
+++ backend/db/seed.sql
@@ -0,0 +1,13 @@
+\c meeting_room_bookings;
+
+INSERT INTO meeting_rooms (name, capacity, floor) VALUES
+('Meeting Room 1', 6, 22),
+('Boardroom 2', 12, 21),
+('Hub', 30, 22);
+
+INSERT INTO bookings (meeting_name, meeting_room_id, start_date, end_date, attendees) VALUES
+('Team Alpha - Scrum Standup', 1, '2021-10-29 10:56:00', '2021-10-29 14:56:00', 'jdoe@email.com, bdylan@email.com'),
+('Star Team - Backlog Refinement', 1, '2021-11-11 11:12:01', '2021-11-11 13:12:01', 'SASchwarzman@email.com, michaelC@email.com'),
+('Project Eureka - Requirements Gathering', 2, '2022-11-09 15:15:21', '2022-11-09 15:45:21', 'JGFinley@email.com, JonathanG@email.com'),
+('Star Team - Backlog Refinement 2', 1, '2022-11-11 05:23:44', '2022-11-11 13:20:44', 'JStecher@email.com, cAnderson@email.com'),
+('Team Blackstone - Scrum Standup Fun', 3, '2022-11-11 05:23:44', '2022-11-11 13:20:44', 'JBaratta@email.com, WayneB@email.com');
