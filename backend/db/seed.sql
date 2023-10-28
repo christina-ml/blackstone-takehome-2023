f556ccf42b37d89e0dd8391940c532ced648ee09
Christina Loiacono <loiaconochristina@gmail.com>
2023-10-28 16:18:41 -0400

    Add more seed data to help with testing endpoints

--- backend/db/seed.sql
+++ backend/db/seed.sql
@@ -10,4 +10,6 @@ INSERT INTO bookings (meeting_name, meeting_room_id, start_date, end_date, atten
 ('Star Team - Backlog Refinement', 1, '2021-11-11 11:12:01', '2021-11-11 13:12:01', 'SASchwarzman@email.com, michaelC@email.com'),
 ('Project Eureka - Requirements Gathering', 2, '2022-11-09 15:15:21', '2022-11-09 15:45:21', 'JGFinley@email.com, JonathanG@email.com'),
 ('Star Team - Backlog Refinement 2', 1, '2022-11-11 05:23:44', '2022-11-11 13:20:44', 'JStecher@email.com, cAnderson@email.com'),
+('Team Beta - Scrum Requirements', 3, '2021-08-29 10:56:00', '2021-08-29 14:56:00', 'PaigeR@email.com, vSawhney@email.com'),
+('Project Cyber - Tech Huddle', 2, '2022-11-08 15:15:21', '2022-11-08 15:45:21', 'JoanS@email.com, nGalakatos@email.com'),
 ('Team Blackstone - Scrum Standup Fun', 3, '2022-11-11 05:23:44', '2022-11-11 13:20:44', 'JBaratta@email.com, WayneB@email.com');
