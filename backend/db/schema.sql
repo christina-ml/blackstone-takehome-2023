3564fea716ff89c06e53fc6f80043cbad16e08d3
Christina Loiacono <loiaconochristina@gmail.com>
2023-10-28 13:41:16 -0400

    Update schema with all fields required except attendees

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -8,8 +8,8 @@ DROP TABLE IF EXISTS meeting_rooms;
 CREATE TABLE meeting_rooms (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) UNIQUE NOT NULL,
-    capacity SMALLINT,
-    floor SMALLINT
+    capacity SMALLINT NOT NULL,
+    floor SMALLINT NOT NULL
 );
 
 DROP TABLE IF EXISTS bookings;
@@ -17,7 +17,7 @@ DROP TABLE IF EXISTS bookings;
 CREATE TABLE bookings (
     id SERIAL PRIMARY KEY,
     meeting_name VARCHAR(255) UNIQUE NOT NULL,
-    meeting_room_id INTEGER REFERENCES meeting_rooms(id) ON DELETE CASCADE,
+    meeting_room_id INTEGER NOT NULL REFERENCES meeting_rooms(id) ON DELETE CASCADE,
     start_date TIMESTAMP NOT NULL,
     end_date TIMESTAMP NOT NULL CHECK (start_date < end_date),
     attendees TEXT
