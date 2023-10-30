56b97958202dfb72a04314733764310c5790c15b
Christina Loiacono <loiaconochristina@gmail.com>
2023-10-30 15:32:14 -0400

    Fetch data for meeting rooms using axios SCSS file created for SingleMeetingRoom.js

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import SingleMeetingRoom from "./SingleMeetingRoom";
+
+const API = process.env.REACT_APP_API_URL;
 
 const Home = () => {
-	return <div>Hello Home page</div>;
+	const [meetingRooms, setMeetingRooms] = useState([]);
+
+	useEffect(() => {
+		axios.get( API + "/meeting-rooms" )
+		.then((res)=>{
+			setMeetingRooms(res.data);
+		}).catch((err)=>{
+		  console.log(err);
+		})
+	  }, []);
+
+	return (
+		<div>
+			{meetingRooms.map((meetingRoom) => {
+              return (
+			  	<SingleMeetingRoom 
+					key={meetingRoom.id} 
+					meetingRoom={meetingRoom}
+				/>
+			  );
+            })}
+		</div>
+	)
 };
 
 export default Home;
--- frontend/src/Components/SingleMeetingRoom.js
+++ frontend/src/Components/SingleMeetingRoom.js
@@ -1,7 +1,23 @@
 import React from "react";
+import "./SingleMeetingRoom.scss";
 
-const SingleMeetingRoom = () => {
-	return <div>SingleMeetingRoom</div>;
+const SingleMeetingRoom = ({meetingRoom}) => {
+	return (
+		<div className="SingleMeetingRoom">
+			<div>
+				ID: {meetingRoom.id}
+			</div>
+			<div>
+				Name: {meetingRoom.name}
+			</div>
+			<div>
+				Capacity: {meetingRoom.capacity}
+			</div>
+			<div>
+				Floor: {meetingRoom.floor}
+			</div>
+		</div>
+	);
 };
 
 export default SingleMeetingRoom;
