51069e792f1d0d7c094e994b9436df27cc5270f4
Christina Loiacono <loiaconochristina@gmail.com>
2023-10-31 18:24:48 -0400

    Fetch all bookings using axios on frontend

--- frontend/src/Components/Bookings.js
+++ frontend/src/Components/Bookings.js
@@ -1,7 +1,35 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import SingleBooking from "./SingleBooking";
+
+const API = process.env.REACT_APP_API_URL;
 
 const Bookings = () => {
-	return <div>Bookings</div>;
+	const [allBookings, setAllBookings] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get(API + '/bookings')
+			.then((res) => {
+				setAllBookings(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
+
+	return (
+		<div>
+			{allBookings.map((booking) => {
+				return (
+					<SingleBooking 
+						key={booking.id} 
+						booking={booking} 
+					/>
+				)
+			})}
+		</div>
+	);
 };
 
 export default Bookings;
