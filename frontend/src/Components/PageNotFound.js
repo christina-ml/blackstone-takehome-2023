a16338abfb302ed729613625b24cac69a86afe6e
Christina Loiacono <loiaconochristina@gmail.com>
2023-10-31 13:01:21 -0400

    Add SCSS and link to return home for PageNotFound

--- frontend/src/Components/PageNotFound.js
+++ frontend/src/Components/PageNotFound.js
@@ -1,7 +1,24 @@
 import React from "react";
+import "./PageNotFound.scss";
+import { Link } from "react-router-dom";
 
 const PageNotFound = () => {
-	return <div>PageNotFound</div>;
+	return (
+	<div className="PageNotFound">
+		<div className="PageNotFound__notFoundMessage">
+			<h2>404 Error - Page Not Found</h2>
+		</div>
+		<div className="PageNotFound__description">
+			We're sorry the page you are looking for does not exist
+		</div>
+		<div  className="PageNotFound_returnHome">
+			<Link to="/">
+					Return Home
+			</Link>
+		</div>
+
+	</div>
+	);
 };
 
 export default PageNotFound;
