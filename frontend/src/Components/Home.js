292f59a883bbd6b0664547dfbd4f09fa5caed654
Christina Loiacono <loiaconochristina@gmail.com>
2023-10-30 21:38:14 -0400

    Add loading state to for get ALL meeting rooms

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,16 +1,20 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import MeetingRooms from "./MeetingRooms";
+import "./Home.scss";
 
 const API = process.env.REACT_APP_API_URL;
 
 const Home = () => {
 	const [meetingRooms, setMeetingRooms] = useState([]);
+	const [loading, setLoading] = useState(false);
 
 	useEffect(() => {
+		setLoading(true);
 		axios.get( API + "/meeting-rooms" )
 		.then((res)=>{
 			setMeetingRooms(res.data);
+			setLoading(false);
 		}).catch((err)=>{
 		  console.log(err);
 		})
@@ -26,6 +30,13 @@ const Home = () => {
 				/>
 			  );
             })}
+
+			{loading && 
+				<div className="loadingView">
+					Loading...
+					<div className="loadingView__spinnerIcon"></div>
+				</div>
+			}
 		</div>
 	)
 };
